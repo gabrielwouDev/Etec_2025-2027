b767533f36456b000188e1a0b0ad6ad333b30674
Gabriel Penha Monteiro <gabrielpenhadev@gmail.com>
2026-05-15 11:23:47 -0300

    exercício right, left, full e cross join
    Added multiple SQL queries and insert statements for exercises involving product and supplier data.

--- 2°-ano/bd2/Primeiro_bimestre/BD_EstoqueV2/DQL.sql
+++ 2°-ano/bd2/Primeiro_bimestre/BD_EstoqueV2/DQL.sql
@@ -13,3 +13,45 @@ SELECT nomeFabricante, SUM(quantidadeProduto) as 'Total quantidade de produtos' 
 	INNER JOIN tbFabricante ON
 		tbProduto.idFabricante = tbFabricante.idFabricante
 			GROUP BY tbFabricante.nomeFabricante
+--exercício 2
+--1)
+INSERT INTO tbFornecedor (nomeFornecedor,contatoFornecedor)
+VALUES
+('Bom Preço','Socrátes'),
+('Negreiros','Platão'),
+('Extra','Júlio César');
+
+--2)
+SELECT nomeFornecedor,descricaoProduto FROM tbProduto
+	INNER JOIN tbFornecedor ON 
+		tbProduto.idFornecedor = tbFornecedor.idFornecedor
+
+--3)
+SELECT nomeFornecedor,descricaoProduto FROM tbProduto
+	LEFT JOIN tbFornecedor ON 
+		tbProduto.idFornecedor = tbFornecedor.idFornecedor
+
+
+SELECT nomeFornecedor,descricaoProduto FROM tbProduto
+	RIGHT JOIN tbFornecedor ON 
+		tbProduto.idFornecedor = tbFornecedor.idFornecedor
+
+--4)
+INSERT INTO tbProduto (descricaoProduto,valorProduto,quantidadeProduto,idFabricante)
+VALUES
+('Coca-Cola 2L',9.50,2200,4),
+('Pepsi twist 2L',11.25,1600,5),
+('Doritos',5.00,280,5);
+
+--5)
+SELECT nomeFornecedor,descricaoProduto FROM tbProduto
+	FULL JOIN tbFornecedor ON 
+		tbProduto.idFornecedor = tbFornecedor.idFornecedor
+
+--6)
+SELECT tbProduto.descricaoProduto, tbFabricante.nomeFabricante FROM tbProduto
+	CROSS JOIN tbFabricante
+		WHERE tbFabricante.idFabricante = 2
+--Resposta: A consulta retornou 13 registros, O que aconteceu foi que o CROSS JOIN criou uma tabela virtual onde todos os produtos são da fabricante P&G.
+
+
