61bc86994c60d4c397fe65eab2257e23b61c0575
Gabriel Penha Monteiro <gabrielpenhadev@gmail.com>
2026-05-15 11:22:26 -0300

    exercício right, left, full e cross join

--- 2°-ano/bd2/Primeiro_bimestre/BD_EstoqueV2/DML.sql
+++ 2°-ano/bd2/Primeiro_bimestre/BD_EstoqueV2/DML.sql
@@ -1,34 +1,34 @@
 INSERT INTO tbCliente (nomeCliente,cpfCliente,emailCliente,sexoCliente,dataNascimentoCliente)
 VALUES
-('Armando JosÈ Santana',12345678900,'armandojsantana@outlook.com','m','21/02/1961'),
+('Armando Jos√© Santana',12345678900,'armandojsantana@outlook.com','m','21/02/1961'),
 ('Sheila Carvalho Leal',45678909823,'scarvalho@g.com.br','f','13/09/1978'),
 ('Carlos Henrique Souza',76598278299,'cherique@g.com.br','m','08/09/1981'),
 ('Maria Aparecida Souza',87365309899,'mapdasouza@outlook.com','f','07/07/1962'),
 ('Adriana Nogueira Silva',76354309388,'drica1977@g.com.br','f','09/04/1977'),
 ('Paulo Henrique Silva',87390123111,'phsilva80@hotmail.com','m','02/02/1987');
 
-SELECT * FROM tbCliente
-
 INSERT INTO tbFabricante (nomeFabricante)
 VALUES
 ('Unilever'),
 ('P&G'),
-('Bunge');
+('Bunge'),
+('Coca-Cola'),
+('Pepsico');
 
 INSERT INTO tbFornecedor (nomeFornecedor,contatoFornecedor)
 VALUES
-('Atacad„o','Carlos Santos'),
+('Atacad√£o','Carlos Santos'),
 ('Assai','Maria Stella'),
-('Rold„o','Paulo CÈsar');
+('Rold√£o','Paulo C√©sar');
 
 INSERT INTO tbProduto (descricaoProduto,valorProduto,quantidadeProduto,idFabricante,idFornecedor)
 VALUES
 ('Amaciante Downy',5.76,1500,2,1),
 ('Amaciante Confort',5.45,2300,1,1),
-('Sab„o em pÛ OMO',5.99,1280,1,2),
+('Sab√£o em p√≥ OMO',5.99,1280,1,2),
 ('Margarina Qually',4.76,2500,3,1),
 ('Salsicha Hot Dog Sadia',6.78,2900,3,2),
-('Mortadela Perdig„o',2.50,1200,3,3),
+('Mortadela Perdig√£o',2.50,1200,3,3),
 ('Hamburger Sadia',9.89,1600,3,1),
 ('Fralda Pampers',36.00,340,2,3),
 ('Xampu Seda',5.89,800,1,2),
