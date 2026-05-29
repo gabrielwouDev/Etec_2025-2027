138fe1092abfe7ae004137c7d67a24e22cb520df
Gabriel Penha Monteiro <gabrielpenhadev@gmail.com>
2026-05-29 11:20:24 -0300

    Add files via upload

---
+++ 2°-ano/bd2/Segundo-Bimestre/bdComercioLivros/DML.sql
@@ -0,0 +1,25 @@
+INSERT INTO tb_Autor(nomeAutor)
+VALUES
+('Machado de Assis'),
+('José de Alencar'),
+('Erico Veríssimo'),
+('José Augusto N. G. Manzano'),
+('Sergio Furgeri')
+
+INSERT INTO tb_Editora(nomeEditora)
+VALUES
+('Cia das Letras'),
+('Saraiva'),
+('Erica'),
+('Scipione')
+
+INSERT INTO tb_Livro(nomeLivro,isbn,numPaginas,codAutor,codEditora)
+VALUES
+('Java 7',1223232,322,4,4),
+('PHP - Guia Prático',7262722,423,4,null),
+('Dom Casmurro',1313223,212,1,1),
+('O alienista',2323222,300,1,null),
+('Olhai os lírios do campo',2232112,321,3,null),
+('Programação de computadores com Java',3434343,211,4,null),
+('Programação web com plataforma JAVA',2323232,341,null,4)
+
