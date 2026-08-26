ddcaedb3be92b8f411638d62fe336a13133401fd
Gabriel Penha Monteiro <gabrielpenhadev@gmail.com>
2026-08-26 08:05:21 -0300

    Add files via upload

---
+++ 2°-ano/pw2/Nodejs/Segundo-Bimestre/Banco-back/DAO/cliente/buscarDados.js
@@ -0,0 +1,87 @@
+import { conexao } from "../conexao.js";
+
+async function buscarEndereco(){
+  const sql = `SELECT * FROM Endereco`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+async function buscarLimite(){
+  const sql = `SELECT * FROM LimiteDeCredito`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+async function buscarCliente(){
+  const sql = `SELECT * FROM Cliente`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+async function buscarProduto(){
+  const sql = `SELECT * FROM Produto`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+async function buscarPedido(){
+  const sql = `SELECT * FROM Pedido`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+async function buscarPedidoProduto(){
+  const sql = `SELECT * FROM Pedido_Produto`
+  
+  const conn = await conexao()
+  try {
+      // Executar a consulta
+      const [rows, fields] = await conn.query(sql);
+      await conn.end()
+      return rows
+    } catch (err) {
+      return err.message
+    }
+}
+
+export {buscarEndereco,buscarLimite,buscarCliente,buscarProduto,buscarPedido,buscarPedidoProduto}
